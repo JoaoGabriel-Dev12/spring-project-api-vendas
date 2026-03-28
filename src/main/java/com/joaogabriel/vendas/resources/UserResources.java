5fa8e890c5618e8382879a9635c75597a2093e4e
Joao Gabriel <joaogabriel.dev12@gmail.com>
2026-03-28 09:53:59 -0300

    H2 database config

--- src/main/java/com/joaogabriel/vendas/resources/UserResources.java
+++ src/main/java/com/joaogabriel/vendas/resources/UserResources.java
@@ -1,5 +1,8 @@
 package com.joaogabriel.vendas.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,8 +15,14 @@ import com.joaogabriel.vendas.entities.User;
 public class UserResources {
 	
 	@GetMapping("/")
-	public ResponseEntity<User> findAll(){
-		User u = new User(1L, "João GAbriel", "joao@email", "80028922", "senha");
-		return ResponseEntity.ok().body(u);
+	public List<User> findAll(){
+		
+		List<User> list = new ArrayList<>();
+		list.add(new User(1L, "João Gabriel", "joao@email", "80028922", "senha"));
+		list.add(new User(1L, "Bianca", "bianca@email", "5637829", "1234"));
+		list.add(new User(1L, "Bolsonaro mito", "bolsonaro@email", "123456789", "globolixo"));
+		list.add(new User(1L, "Osama Bin Laden", "bomba@email", "87879871", "torresgemeas"));
+		
+		return list;
 	}
 }
