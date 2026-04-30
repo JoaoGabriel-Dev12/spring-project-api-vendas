a4b871677ad6ecc18f4f932211e90c69c89ff5fc
João Gabriel Eugênio Amorim <joaogabriel.dev12@gmail.com>
2026-04-30 16:31:30 -0300

    Add CrossOrigin import to UserResources

--- src/main/java/com/joaogabriel/vendas/resources/UserResources.java
+++ src/main/java/com/joaogabriel/vendas/resources/UserResources.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.joaogabriel.vendas.entities.User;
